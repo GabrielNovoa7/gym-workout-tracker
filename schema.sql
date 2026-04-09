0e51e06596aa062c3e3a276deb785b3742023a72
GabrielNovoa7 <gabriel.novoax@gmail.com>
2026-04-09 13:58:18 -0500

    Refactor SQL schema to improve table structure and naming consistency

--- schema.sql
+++ schema.sql
@@ -1,19 +1,29 @@
 -- file used to hold sql commands
-
 --exercise table to hold info about each exercise
+CREATE TABLE IF NOT EXISTS users(
+    user_id INT,
+    user_name VARCHAR(20)
+);
 CREATE TABLE IF NOT EXISTS exercises(
     exercise_id INT PRIMARY KEY,
     exercise_name VARCHAR(50) NOT NULL,
-    muscle_group VARCHAR(20)
+    primary_muscle_group VARCHAR(20)
 );
 CREATE TABLE IF NOT EXISTS workouts(
     workout_id INT PRIMARY KEY,
-    workout_date DATE
+    workout_date DATE,
+    user_id INT,
+    note VARCHAR(500)
 );
-CREATE TABLE IF NOT EXISTS set_info(
-    set_id INT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS workout_exercises(
+    workout_exercise_id INT PRIMARY KEY,
     workout_id INT,
-    exercise_id INT,
-    weight_lb INT,
-    reps INT NOT NULL 
+    exercise_id INT
+);
+CREATE TABLE IF NOT EXISTS sets_info(
+    set_id INT PRIMARY KEY,
+    workout_exercise_id INT,
+    set_number INT,
+    weight_lb DECIMAL,
+    reps INT
 );

ff134daffddb7b535d4306af2e3ea5fe493754ba
GabrielNovoa7 <217294271+GabrielNovoa7@users.noreply.github.com>
2026-04-17 17:19:34 -0500

    Refactor data processing in main.py: streamline columns and enhance output formatting

--- src/main.py
+++ src/main.py
@@ -1,15 +1,20 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from datetime import datetime
 
 # load workout data into variable
 data = pd.read_csv("data/workouts.csv")
+#convert start_time and end_time columns from str to datetime datatypes
 data["start_time"] = pd.to_datetime(data["start_time"])
 data["end_time"] = pd.to_datetime(data["end_time"])
-print(data.dtypes)
 
+#create volume column 
+data["set_volume"] = data["weight_lbs"] * data["reps"]
 #subset data to only useful data 
-revisedData = ["title","start_time","end_time","exercise_title","superset_id","set_index","set_type","weight_lbs","reps"]
-print(data[revisedData])
-print(data.sort_values("start_time", ascending=True))
+revisedData = ["title","start_time","end_time","exercise_title","weight_lbs","reps","set_volume"]
+print(data[revisedData].sort_values("start_time", ascending=True))
+
+
+#set the start_time as the index
+revisedData2 = data[revisedData]
+revisedData2.groupby(["start_time","exercise_title"])[["weight_lbs","reps","set_volume"]]
